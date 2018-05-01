542414f2fe99dd5f657071f319c226f61fd7b49e
Zain <zainelabidine.bensaleh@esprit.tn>
2018-05-01 17:33:08 +0100

    add controllers json and add entité gouv et ville et leurs controlleurs

---
+++ src/ProfilBundle/Controller/GouvernoratController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ProfilBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class GouvernoratController extends Controller
+{
+    public function alljsonAction()
+    {
+        $em= $this->getDoctrine()->getManager();
+        $utilisateurs=$em->getRepository("EntiteBundle:Gouvernorat")
+            ->findAll();
+        $serializer=new Serializer([new ObjectNormalizer()]);
+        $formatted=$serializer->normalize($utilisateurs);
+        return new JsonResponse($formatted);
+    }
+}
